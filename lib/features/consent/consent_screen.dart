2e929daacb63cb8c38ff76a57ba92027a27ba57d
Supakorn <supakorn0416@gmail.com>
2025-09-29 17:16:21 +0700

    feat: update consent screen

--- lib/features/consent/consent_screen.dart
+++ lib/features/consent/consent_screen.dart
@@ -5,13 +5,17 @@ class ConsentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Terms of Service")),
+      appBar: AppBar(
+          title: const Text(
+        "Terms of Service",
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      )),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Text(
+              const Text(
                   'The Application Usage Policy (Mock Version), last updated on 23/11/24, outlines the rules and guidelines for using the [Your App Name] application (“the App”) to ensure all users enjoy a safe, reliable, and secure experience; users must use the App only for lawful purposes and avoid misuse that could harm its performance, security, or other users, while sharing false, harmful, or illegal content is strictly prohibited; users are responsible for maintaining the confidentiality of their account information, should note that the App team will never request passwords through email or unofficial channels, and must report any unauthorized use immediately; the App may collect basic user information such as name, email, and usage data to improve services, but personal data will not be sold or shared with third parties without consent, with further details available in the Privacy Policy; users are restricted from reverse engineering, copying, or distributing the App without permission, using the App to send spam, harassment, or harmful content, or violating intellectual property rights; and for any questions about this policy, users may contact [support@example.com].'),
               ListTileTheme(
                 horizontalTitleGap: 0,
@@ -19,7 +23,7 @@ class ConsentScreen extends StatelessWidget {
                     visualDensity: VisualDensity.compact,
                     splashRadius: 18,
                     side: const BorderSide(width: 1, color: Color(0xFFD9D9D9)),
-                    activeColor: Color(0xFF8532FF),
+                    activeColor: const Color(0xFF8532FF),
                     contentPadding: EdgeInsets.zero,
                     title: const Text(
                       'Accept the terms of service',
@@ -48,8 +52,8 @@ class ConsentScreen extends StatelessWidget {
             ),
             backgroundColor: Theme.of(context).primaryColor,
           ),
-          child: Text(
-            'Next',
+          child: const Text(
+            'Confirm',
             style: TextStyle(
                 fontSize: 16, fontWeight: FontWeight.w700, color: Colors.white),
           ),
