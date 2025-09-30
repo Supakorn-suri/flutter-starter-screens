7f711193a97c3fdde342e7b898f6d8996ea8ffc0
Supakorn <supakorn0416@gmail.com>
2025-09-30 14:45:13 +0700

    feat: update home appbar text style

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -16,7 +16,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Screen")),
+      appBar: AppBar(title: const Text("Home",
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      )),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: SingleChildScrollView(
